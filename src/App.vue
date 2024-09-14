006d811417bc0262e16c0fa82f39c6d94089f3b4
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-09-13 20:31:03 -0400

    add skeleton for navbar and sidebar, get color wheel initial image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="main-container">
-    <div class="navBar">
-      <div class="save-image"></div>
-      <div class="open-image"></div>
-      <div class="brush-size"></div>
-      <div class="brush-type"></div>
-      <div class="brush-shapes"></div>
-    </div>
-    <canvas id="paintin-canvas" class="paintin-canvas" @mousedown="setClickDown($event)" @mouseup="setClickUp($event)" @mousemove="hoverMouse($event)">
-    </canvas>
+    <main class="content-container">
+      <div class="navbar">
+        <div class="save-image navbar-button">Save</div>
+        <div class="open-image navbar-button">Open</div>
+        <div class="brush-sizes navbar-list">
+          <input type="number" name="brush-size-number-input" id="brush-size-number-input" v-model="brushSize" step="1" min="2" max="100">
+          <input type="range" name="brush-size-range-input" id="brush-size-range-input" v-model="brushSize" step="1" min="2" max="100">
+        </div>
+        <ul class="brush-types navbar-list">
+          <li class="type-brush navbar-button">Pen</li>
+          <li class="type-brush navbar-button">Eraser</li>
+        </ul>
+        <ul class="brush-shapes navbar-list">
+          <li class="shape-brush">Circle</li>
+          <li class="shape-brush">Triangle</li>
+          <li class="shape-brush">Square</li>
+        </ul>
+      </div>
+  
+      <canvas id="paintin-canvas" class="paintin-canvas" @mousedown="setClickDown($event)" @mouseup="setClickUp($event)" @mousemove="hoverMouse($event)">
+      </canvas>
+    </main>
+
+    <aside class="sidebar">
+      <div class="color-picker sidebar-option">
+        <input type="color" name="color-picker-sidebar" id="color-picker-sidebar" v-model="color" @mouseover="test($event)">
+      </div>
+    </aside>
   </div>
 </template>
 
@@ -18,11 +37,12 @@ export default {
     return {
       canvas: null,
       ctx: null,
-      canvasSize: 800,
+      canvasSizeX: 1580,
+      canvasSizeY: 880,
       isDrawing: false,
       brushSize: 5,
       mousePos: [0,0],
-      fillStyle: 'red'
+      color: '#ff0000'
     }
   },
   computed: {
@@ -34,12 +54,16 @@ export default {
   mounted() {
     this.canvas = document.getElementById('paintin-canvas')
     this.ctx = this.canvas.getContext('2d')
-    this.canvas.height = this.canvasSize
-    this.canvas.width = this.canvasSize
+    this.canvas.height = this.canvasSizeY
+    this.canvas.width = this.canvasSizeX
     this.penMode()
   },
   methods: {
+    test(event){
+      console.log(event.target.value);
+    },
     setClickDown(event){
+      console.log('click');
       this.isDrawing = true
       this.draw(this.mousePos)
     },
@@ -54,14 +78,14 @@ export default {
     },
     draw(event){
       this.ctx.lineWidth = this.brushSize
-      this.ctx.strokeStyle = this.fillStyle
-      this.ctx.fillStyle = this.fillStyle
+      this.ctx.strokeStyle = 'red'
+      this.ctx.fillStyle = 'red'
       this.ctx.lineCap = 'round'
       this.ctx.lineJoin = 'round'
-      this.ctx.lineTo(event.offsetX, event.offsetY)
+      this.ctx.lineTo(event.clientX - this.canvas.offsetLeft, event.clientY - this.canvas.offsetTop)
       this.ctx.stroke()
       this.ctx.beginPath()
-      this.ctx.moveTo(event.offsetX, event.offsetY)
+      this.ctx.moveTo(event.clientX - this.canvas.offsetLeft, event.clientY - this.canvas.offsetTop)
     },
     eraserMode(){
       this.ctx.globalCompositeOperation = 'destination-out'
@@ -69,7 +93,7 @@ export default {
     },
     penMode(){
       this.ctx.globalCompositeOperation = 'source-over';
-      this.ctx.strokeStyle = this.fillStyle;
+      this.ctx.strokeStyle = 'red';
     }
   },
 }
